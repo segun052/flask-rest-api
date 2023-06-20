9f8d653038d92209b02c2e097cd4fb7bd1b027d1
Segun Adetunji <segunvictor052@gmail.com>
2023-06-20 10:32:24 +0100

    debug(debug):fixing render issues

--- models/user.py
+++ models/user.py
@@ -5,5 +5,6 @@ class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80),  nullable=False)
+    username = db.Column(db.String(256), unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String(256),  nullable=False)
